a3637c73833cc9ddc3dd09c9050206a36568da6a
maodaisuki <nyanyanyapu@gmail.com>
2024-05-05 11:20:25 +0800

    fix: request.url

--- src/app/api/search/getSpecialCount/route.ts
+++ src/app/api/search/getSpecialCount/route.ts
@@ -2,7 +2,6 @@ import { getPostFile, getSpecialPosts } from "@/lib/api";
 
 export async function GET(request: any) {
     console.log("It works!");
-    const { searchParams }: { searchParams : URLSearchParams}  = new URL(request.url);
     const query: string = request.query.string.toString() || "" ;
     const postFiles = getSpecialPosts(query, 1, Number.MAX_SAFE_INTEGER);
     let allCount = postFiles.length / (parseInt(process.env.PAGINATION_POST_LIMIT!) || 20);
--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,7 +2,6 @@ import { getSpecialPosts } from "@/lib/api";
 
 export async function GET(request: any) {
   console.log("It works!");
-  const { searchParams }: { searchParams : URLSearchParams}  = new URL(request.url);
   const limit = parseInt(process.env.PAGINATION_POST_LIMIT!) || 20;
   const page:number = parseInt(request.query.page.toString()) || 1 ;
   const query: string = request.query.string.toString() || "" ;
